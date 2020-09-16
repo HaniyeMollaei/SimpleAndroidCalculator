d5e1d4370f4b0d4e9b877c5da854eb3cb388a7d0
haniye molaei <haniyemolaei1378@gmail.com>
2020-09-16 22:51:51 +0430

    Design a simple UI

--- app/src/main/java/com/example/simplecalculator/MainActivity.kt
+++ app/src/main/java/com/example/simplecalculator/MainActivity.kt
@@ -2,10 +2,12 @@ package com.example.simplecalculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+    fun onClick(view : View){}
 }
